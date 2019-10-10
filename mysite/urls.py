157961b13e238afbac7df5e215bd6fa869ecc02e
DimaLat <1ap.pr@mail.ru>
2019-10-11 00:59:20 +0300

    login view

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
     path('account/', include('accounts.urls')),
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
-    path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name="logout"),
+    
 
 ]
 
